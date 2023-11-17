41590a8d19383dea68d36dde777c1a0e5ac3ddfd
jaesoon3737 <92350740+jaesoon3737@users.noreply.github.com>
2023-11-17 13:09:13 +0900

    header 작업 및 테마 작업 중

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias = {
+                ...config.resolve.alias,
+                '@mui/styled-engine': '@mui/styled-engine-sc',
+        };
+        return config;
+    }
+}
 module.exports = nextConfig
